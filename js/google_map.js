2a313cf5c60ca73f6fb27a0cdb937a4549cd2bf5
ezeutno <ivan.suratno@gmail.com>
2021-05-17 15:42:22 +0700

    Add google api for socaas.github.io

--- js/google_map.js
+++ js/google_map.js
@@ -30,7 +30,7 @@ function init() {
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
     
-    var addresses = ['Brooklyn'];
+    var addresses = ['Wollongong'];
 
     for (var x = 0; x < addresses.length; x++) {
         $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
